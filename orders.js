9c9a0cd5101196e8e7afc002020d5337f7a2c7f3
Dhe-Reigner <martinkagema01@gmail.com>
2024-10-26 16:16:32 +0300

    Initial commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName:'3kg Managu',
+        productNumber:'43435',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'4kg Onions',
+        productNumber:'64453',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'13kg Cabbages',
+        productNumber:'43456',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'7kg Tomatoes',
+        productNumber:'12355',
+        paymentStatus:'Paid',
+        shipping:'Deliverd'
+    },
+    {
+        productName:'12 Spinach',
+        productNumber:'86479',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    }
+]
